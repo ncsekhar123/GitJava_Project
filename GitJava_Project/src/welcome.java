be4b2026a7ba5b36dcd4115a73b2cf1d4d636654
chandra <nchandras123@gmail.com>
2023-08-27 21:11:33 +0530

    First Commit

---
+++ GitJava_Project/src/welcome.java
@@ -0,0 +1,14 @@
+
+public class welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Git First Commit");
+		//System.out.println("Git Second Commit");
+		//System.out.println("Git Third Commit");
+
+
+	}
+
+}
