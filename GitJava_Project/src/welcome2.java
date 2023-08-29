a40274c587471a6b70dc37078c08f265323a5967
chandra <nchandras123@gmail.com>
2023-08-29 21:09:32 +0530

    sdfsdfsdfs

--- GitJava_Project/src/welcome2.java
+++ GitJava_Project/src/welcome2.java
@@ -3,7 +3,7 @@ public class welcome2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("New JAVA Class");
+		System.out.println("New JAVA Class  ");
 	}
 
 }
