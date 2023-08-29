787d0d879263ab1042241376fc645167f81aecef
chandra <nchandras123@gmail.com>
2023-08-29 20:52:45 +0530

    test

--- GitJava_Project/src/welcome.java
+++ GitJava_Project/src/welcome.java
@@ -3,15 +3,7 @@ public class welcome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("Git First Commit");
-		System.out.println("Git Second Commit");
-		System.out.println("Git Third Commit");
-		System.out.println("Git Final Commit");
-		System.out.println("Git zvcx Commit");
-		System.out.println("Git New123 Commitssddd  ");
-
-
+		System.out.println("Commit 1 2 3 4");
 	}
 
 }
